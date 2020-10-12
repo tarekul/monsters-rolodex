10651c52dbeb96392c579d53d33d243b79284421
Pursuit <rahin6810@gmail.com>
2020-10-12 18:17:20 -0400

    Updates

---
+++ precache-manifest.196512afec8e15205493453d6f5e3340.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f126f808e812cf1e288d53b7231434",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "90831102d7ce44706d64",
+    "url": "/monsters-rolodex/static/css/main.e970b992.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90831102d7ce44706d64",
+    "url": "/monsters-rolodex/static/js/main.468e5ee6.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
